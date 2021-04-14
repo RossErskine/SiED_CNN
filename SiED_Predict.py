e8f7f62ac6f2c1e77dd4d1e7d011c11703a01d9c
Ross.Erskine <46631932+RossErskine@users.noreply.github.com>
2021-04-14 10:19:25 +0100

    Add files via upload
    added is _unique function does not work but want to keep incase

--- SiED_Predict.py
+++ SiED_Predict.py
@@ -3,6 +3,7 @@ import numpy as np
 from keras.models import load_model
 import cv2
 import matplotlib.pyplot as plt 
+from Symbol_classes import Symbol_info, Lines
 
 # load model
 model = load_model('D:/DDocuments/Python/Practical_computer_vision/SiED_CNN_3.h5')
@@ -27,39 +28,62 @@ SiED_INSTANCE_CATEGORY_NAMES = ['Arrowhead', 'Arrowhead + Triangle', 'Barred Tee
 (SwinW, SwinH) = (100, 100) 
 (imgW, imgH) = (1010, 1920)
 
-image = Image.open(image) # Load the image
-threshold = 0.3
-pred_boxes =[]
-pred_class = []
+image = 'D:/DDocuments/Python/Practical_computer_vision/P&ID plan.png'
 
+THRESHOLD = 0.3
+SYMBOLS = []
 
-for x in range(0,1920-100,10):
-    for y in range(0, 1010-100, 10):
-        img = image.crop((x, y, x+SwinW, y+SwinH)) # crop to 100x100 to fit into model
-        img = img.convert('L')# converts image to a single channel
-        img = np.array(img)
-        img = img.reshape((-1, 100, 100, 1))
-        img = img.astype('float32') / 255.0
+def is_unique(symbol):
+    flag = True 
+    for s in SYMBOLS:
+        if symbol.box[0][0] > s.box[0][0] -50 and symbol.box[0][0] < s.box[0][0] +50: # symbol x betwwen 50 0f s x
+            if symbol.box[0][1] > s.box[0][1] +50 and symbol.box[0][1] < s.box[0][1] +50 :# symbol y betwwen 50 0f s y
+                if symbol.label == s.label:
+                    if symbol.score < s.score:
+                        flag = False
+                        return False
+                    else:
+                        del s
+                        flag = True
+                        
+    if flag == True:
+        return True
+    
+     
         
-        pred = model(img)
-        pred_h = np.argmax(pred)    # get prediction score
-        clsf = SiED_INSTANCE_CATEGORY_NAMES[pred_h]  # Get the class name
-        box = ((x, y), (x+SwinW, y+SwinH))  # Bounding boxes
-        if pred[0,pred_h] > threshold:
-            pred_boxes.append(box)
-            pred_class.append(clsf)
-        
-final_image = cv2.imread('D:/DDocuments/Python/Practical_computer_vision/P&ID plan.png') # Read image with cv2
-for i in range(len(pred_boxes)):
-    cv2.rectangle(final_image, pred_boxes[i][0], pred_boxes[i][1],color=(0, 255, 0), thickness=2) # Draw Rectangle with the coordinates
-    cv2.putText(final_image,pred_class[i], pred_boxes[i][0],  cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0),thickness=2) # Write the prediction class
-plt.figure(figsize=(20,30)) # display the output image
-plt.imshow(final_image)
-plt.xticks([])
-plt.yticks([])
-plt.show()
-plt.show()         
+    
+    
+
+def predict(image):
+    for x in range(0,1920-100,10):
+        for y in range(0, 1010-100, 10):
+            img = image.crop((x, y, x+SwinW, y+SwinH)) # crop to 100x100 to fit into model
+            img = img.convert('L')# converts image to a single channel
+            img = np.array(img)
+            img = img.reshape((-1, 100, 100, 1))
+            img = img.astype('float32') / 255.0
+            
+            pred = model(img) # get prediction scores
+            high_score = np.argmax(pred)    # get highest prediction score
+            newSymbol = Symbol_info(x,y, SiED_INSTANCE_CATEGORY_NAMES[high_score], high_score)# Get the class name
+            if pred[0,high_score] > THRESHOLD:
+                if is_unique(newSymbol):
+                    SYMBOLS.append(newSymbol)
+ 
+def symbol_detection(image):        
+    final_image = cv2.imread(image) # Read image with cv2
+    image = Image.open(image) # Load the image
+    predict(image)
+    for i in range(len(SYMBOLS)):
+        cv2.rectangle(final_image, SYMBOLS[i].box[0], SYMBOLS[i].box[1],color=(0, 255, 0), thickness=2) # Draw Rectangle with the coordinates
+        cv2.putText(final_image,SYMBOLS[i].label, SYMBOLS[i].box[0],  cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0),thickness=2) # Write the prediction class
+    plt.figure(figsize=(20,30)) # display the output image
+    plt.imshow(final_image)
+    plt.xticks([])
+    plt.yticks([])
+    plt.show()
+    plt.show()         
            
-         
+symbol_detection(image)      
 
 
