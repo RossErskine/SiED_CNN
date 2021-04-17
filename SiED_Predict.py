6b140fe501293e8817091fdb0e6bd03ea747a992
Ross.Erskine <46631932+RossErskine@users.noreply.github.com>
2021-04-17 14:38:45 +0100

    added nms function
    tensorflow non-max-suppression

--- SiED_Predict.py
+++ SiED_Predict.py
@@ -1,17 +1,12 @@
-from PIL import Image
-import numpy as np
+
 from keras.models import load_model
-import cv2
-import matplotlib.pyplot as plt 
-from Symbol_classes import Symbol_info, Lines
 
 # load model
 model = load_model('D:/DDocuments/Python/Practical_computer_vision/SiED_CNN_3.h5')
 
 # Compilling the model
 model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics= ['acc'])
-
-image = ('D:/DDocuments/Python/Practical_computer_vision/P&ID plan.png')   
+  
 
 # define class names
 SiED_INSTANCE_CATEGORY_NAMES = ['Arrowhead', 'Arrowhead + Triangle', 'Barred Tee', 'continuity Label',
@@ -24,35 +19,22 @@ SiED_INSTANCE_CATEGORY_NAMES = ['Arrowhead', 'Arrowhead + Triangle', 'Barred Tee
                                 'Valve Ball', 'Valve Butterfly', 'Valve Check', 'Valve Gate Through Conduit',
                                 'Valve Globe', 'Valve Plug', 'Valve Slab Gate', 'Vessel']
 
+
+from PIL import Image
+image = 'D:/DDocuments/Python/Practical_computer_vision/P&ID plan.png'
+
+# Global variables
+THRESHOLD = 0.3
+SYMBOLS = [] # an empty list for Symbol_info class
+
 # Sliding window size & Image size
 (SwinW, SwinH) = (100, 100) 
 (imgW, imgH) = (1010, 1920)
 
-image = 'D:/DDocuments/Python/Practical_computer_vision/P&ID plan.png'
-
-THRESHOLD = 0.3
-SYMBOLS = []
-
-def is_unique(symbol):
-    flag = True 
-    for s in SYMBOLS:
-        if symbol.box[0][0] > s.box[0][0] -50 and symbol.box[0][0] < s.box[0][0] +50: # symbol x betwwen 50 0f s x
-            if symbol.box[0][1] > s.box[0][1] +50 and symbol.box[0][1] < s.box[0][1] +50 :# symbol y betwwen 50 0f s y
-                if symbol.label == s.label:
-                    if symbol.score < s.score:
-                        flag = False
-                        return False
-                    else:
-                        del s
-                        flag = True
-                        
-    if flag == True:
-        return True
-    
      
         
-    
-    
+import numpy as np  
+from Symbol_classes import Symbol_info   # own class in Symbol_classes.py
 
 def predict(image):
     for x in range(0,1920-100,10):
@@ -67,16 +49,39 @@ def predict(image):
             high_score = np.argmax(pred)    # get highest prediction score
             newSymbol = Symbol_info(x,y, SiED_INSTANCE_CATEGORY_NAMES[high_score], high_score)# Get the class name
             if pred[0,high_score] > THRESHOLD:
-                if is_unique(newSymbol):
-                    SYMBOLS.append(newSymbol)
- 
+                SYMBOLS.append(newSymbol)
+
+import cv2
+import matplotlib.pyplot as plt   
+import tensorflow as tf
+
+# non-maximum suppression
+boxes = []
+scores = []
+
+def nms():
+    for i in range(len(SYMBOLS)):
+        boxes.append(SYMBOLS[i].box)
+        scores.append(SYMBOLS[i].score)
+
+    tf.convert_to_tensor(boxes,dtype=tf.float32)
+    tf.convert_to_tensor(scores,dtype=tf.float32)
+
+    selected_indices = tf.image.non_max_suppression(boxes, scores, 20, iou_threshold=0.5)
+    selected_boxes = tf.gather(boxes, selected_indices)
+    
+    return selected_boxes
+
+
 def symbol_detection(image):        
     final_image = cv2.imread(image) # Read image with cv2
     image = Image.open(image) # Load the image
     predict(image)
-    for i in range(len(SYMBOLS)):
-        cv2.rectangle(final_image, SYMBOLS[i].box[0], SYMBOLS[i].box[1],color=(0, 255, 0), thickness=2) # Draw Rectangle with the coordinates
-        cv2.putText(final_image,SYMBOLS[i].label, SYMBOLS[i].box[0],  cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0),thickness=2) # Write the prediction class
+    select_boxes = nms()
+    tf.dtypes.cast(boxes,tf.int32)
+    for i in range(len(select_boxes)):
+        cv2.rectangle(final_image, (select_boxes[i][0],select_boxes[i][1]),(select_boxes[i][2],select_boxes[i][3]),color=(0, 255, 0), thickness=2) # Draw Rectangle with the coordinates
+        #cv2.putText(final_image,SYMBOLS[i].label, SYMBOLS[i].box[0],  cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0),thickness=2) # Write the prediction class
     plt.figure(figsize=(20,30)) # display the output image
     plt.imshow(final_image)
     plt.xticks([])
