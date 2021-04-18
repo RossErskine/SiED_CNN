67807efa5060c150f295bf28996c4f612abeb5f6
Ross.Erskine <46631932+RossErskine@users.noreply.github.com>
2021-04-18 12:47:10 +0100

    add pyramid function
    pyramid function changes size of original image then predicts on each image

--- SiED_Predict.py
+++ SiED_Predict.py
@@ -24,7 +24,7 @@ from PIL import Image
 image = 'D:/DDocuments/Python/Practical_computer_vision/P&ID plan.png'
 
 # Global variables
-THRESHOLD = 0.3
+THRESHOLD = 0.6
 SYMBOLS = [] # an empty list for Symbol_info class
 
 # Sliding window size & Image size
@@ -33,12 +33,14 @@ SYMBOLS = [] # an empty list for Symbol_info class
 
      
         
-import numpy as np  
+import numpy as np 
 from Symbol_classes import Symbol_info   # own class in Symbol_classes.py
 
-def predict(image):
-    for x in range(0,1920-100,10):
-        for y in range(0, 1010-100, 10):
+
+def predict(image, scale, power):
+    #image = Image.open(image) # Load the image
+    for x in range(0,image.size[1]-100,10):    #width
+        for y in range(0, image.size[0]-100, 10):  #height
             img = image.crop((x, y, x+SwinW, y+SwinH)) # crop to 100x100 to fit into model
             img = img.convert('L')# converts image to a single channel
             img = np.array(img)
@@ -47,9 +49,31 @@ def predict(image):
             
             pred = model(img) # get prediction scores
             high_score = np.argmax(pred)    # get highest prediction score
-            newSymbol = Symbol_info(x,y, SiED_INSTANCE_CATEGORY_NAMES[high_score], high_score)# Get the class name
+            newSymbol = Symbol_info(x,y, SiED_INSTANCE_CATEGORY_NAMES[high_score], high_score, scale, power)# Get the class name
             if pred[0,high_score] > THRESHOLD:
                 SYMBOLS.append(newSymbol)
+
+def pyramid(image, scale=1.1, minSize=(1000, 1000)):
+     power = 1
+     while True:
+        # compute the new dimensions of the image and resize it
+        w = int(image.size[1] / scale)
+        h = int(image.size[0] / scale)
+        image = image.resize((h, w))
+        predict(image, scale, power)
+        power += 1
+        #if the resized image does not meet the supplied minimum
+        #size, then stop constructing the pyramid
+        if image.size[0] < minSize[1] or image.size[1] < minSize[0]:
+           break
+    
+   
+
+def selectSymbols(selected_indices):
+    selected_symbols = []
+    for i in selected_indices:
+        selected_symbols.append(SYMBOLS[i])
+    return selected_symbols
 
 import cv2
 import matplotlib.pyplot as plt   
@@ -67,21 +91,21 @@ def nms():
     tf.convert_to_tensor(boxes,dtype=tf.float32)
     tf.convert_to_tensor(scores,dtype=tf.float32)
 
-    selected_indices = tf.image.non_max_suppression(boxes, scores, 20, iou_threshold=0.5)
-    selected_boxes = tf.gather(boxes, selected_indices)
+    selected_indices = tf.image.non_max_suppression(boxes, scores, 25, iou_threshold=0.1)
+    selected_symbols = selectSymbols(selected_indices)
     
-    return selected_boxes
+    return selected_symbols
 
 
 def symbol_detection(image):        
     final_image = cv2.imread(image) # Read image with cv2
+    #image = cv2.imread(image)
     image = Image.open(image) # Load the image
-    predict(image)
-    select_boxes = nms()
-    tf.dtypes.cast(boxes,tf.int32)
-    for i in range(len(select_boxes)):
-        cv2.rectangle(final_image, (select_boxes[i][0],select_boxes[i][1]),(select_boxes[i][2],select_boxes[i][3]),color=(0, 255, 0), thickness=2) # Draw Rectangle with the coordinates
-        #cv2.putText(final_image,SYMBOLS[i].label, SYMBOLS[i].box[0],  cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0),thickness=2) # Write the prediction class
+    pyramid(image) #pyramids image and predicts
+    select_symbols = nms() # non-max suppresion
+    for i in range(len(select_symbols)):
+        cv2.rectangle(final_image, (select_symbols[i].box[0],select_symbols[i].box[1]),(select_symbols[i].box[2],select_symbols[i].box[3]),color=(0, 255, 0), thickness=2) # Draw Rectangle with the coordinates
+        cv2.putText(final_image,select_symbols[i].label, (select_symbols[i].box[0],select_symbols[i].box[1]),  cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0),thickness=2) # Write the prediction class
     plt.figure(figsize=(20,30)) # display the output image
     plt.imshow(final_image)
     plt.xticks([])
