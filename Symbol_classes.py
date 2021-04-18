4d402d6fecd5cdfb26a0ee6a61321cb4aa7e4cef
Ross.Erskine <46631932+RossErskine@users.noreply.github.com>
2021-04-18 12:50:11 +0100

    updated class.box with scale

--- Symbol_classes.py
+++ Symbol_classes.py
@@ -1,18 +1,15 @@
-from collections import namedtuple
-
-Point = namedtuple('x', 'y')
 
 class Symbol_info:
     """ to store detected symbol information """
     
-    def __init__(self, x, y, label):
-        self.x = x
-        self.y = y
+    def __init__(self, x, y, label, score, scale, power):
+        x = int(x * (scale ** power)) # moves position depending scale for final image
+        y = int(y * (scale ** power))
+        self.box = (x ,y,(x+ int(100 / (scale ** power))),(y+ int(100 / (scale ** power))))
         self.label = label
+        self.score = score
         self.text =""
-        self.box = ((x,y),( x+100, y +100))
-    
-    
+        
     def setText(self, text):
         self.text = text
         
@@ -26,11 +23,9 @@ class Lines:
     label_names = []  
     
     def __init__(self, sx, sy, ex, ey):
-        self.sx = sx
-        self.sy = sy
-        self.ex = ex
-        self.ey = ey
+        self.line = ((sx, sy),(ex,ey))
         
-     
+    def addText(self, label):
+         self.label_names.append(label)
     
     
